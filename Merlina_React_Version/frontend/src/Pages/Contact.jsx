c3923bbcddb0e1df63483d689ecf85653ab9191b
joffrea18 <joffreantonioariasmarquez@gmail.com>
2024-08-31 18:41:37 +0200

    login&contactPageCreates

--- Merlina_React_Version/frontend/src/Pages/Contact.jsx
+++ Merlina_React_Version/frontend/src/Pages/Contact.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import Button from 'react-button-component';
 
 const Contact = () => {
     return (
         <div>
+           <form action="">
+            <fieldset id='contact'>
+                <h2>Contact Us</h2>
+                    <label htmlFor="">
+                        <input type="text" 
+                        name="name" 
+                        required 
+                        placeholder='name *'/>
+                    </label>
+                    <label htmlFor="">
+                        <input type="text"
+                        name="lastname" 
+                        required
+                        placeholder='lastname *'
+                        />
+                    </label>
+                    <label htmlFor="">
+                        <input type="email"
+                        name="email" 
+                        required
+                        placeholder='email * example@email.com'
+                        />
+                    </label>
+                    <label htmlFor="">
+                        <input type="textarea"
+                        name="description" 
+                        required
+                        placeholder='Please indicate your requirement here.'
+                        />
+                    </label>
+                    <div className="contact">
+                        <Button >Send Request</Button>
+                    </div>
+            </fieldset>
+
+           </form>
             
         </div>
     );
--- Merlina_React_Version/frontend/src/Pages/LoginSingUp.jsx
+++ Merlina_React_Version/frontend/src/Pages/LoginSingUp.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Button from 'react-button-component';
+import { Link } from 'react-router-dom';
 
 const LoginSingUp = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <form >
+          <fieldset id='login'>
+        <h2>Your Account</h2>
+            <label>
+              <input
+              name="email"
+              type="email"
+              required
+              placeholder='Email*'
+              />
+            </label>
+            <label>
+              <input
+              name="password"
+              type="password"
+              required
+              placeholder='Password*'
+              />
+            </label>
+            {/* <li>Token: {token}</li> */}
+            <div className="login">
+            <Button >Login</Button>
+            </div>
+            <p>
+              ¿You don't have account?
+              <br/>
+              <Link to="/contact">Regist</Link>
+              <br/>
+              <br/>
+              <Link to="/">Home</Link>
+            </p>
+          </fieldset>
+        </form>
+        </>
     );
 }
 
